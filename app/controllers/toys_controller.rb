babd91450829bf7c18e8e0d5d87e33890972fb6b
Joshua Campos <jdeveloper1203@gmail.com>
2022-11-07 16:53:05 -0500

    commit tree 4d885666f1705f30f42ce5f08737c010b2553c86

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -1,5 +1,6 @@
 class ToysController < ApplicationController
   wrap_parameters format: []
+  rescue_from ActiveRecord::RecordNotFound, with: :error_toy
 
   def index
     toys = Toy.all
@@ -7,13 +8,14 @@ class ToysController < ApplicationController
   end
 
   def create
-    toy = Toys.create(toy_params)
+    toy = Toy.create(toy_params)
     render json: toy, status: :created
   end
 
   def update
     toy = Toy.find_by(id: params[:id])
     toy.update(toy_params)
+    render json: toy
   end
 
   def destroy
@@ -26,6 +28,10 @@ class ToysController < ApplicationController
   
   def toy_params
     params.permit(:name, :image, :likes)
+  end
+
+  def error_toy
+    render json: {error: "Toy not found"}, status: :not_found
   end
 
 end
